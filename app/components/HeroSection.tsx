13a78dbd3d6e79a62a331050880fec7b0df0d202
jularanja <juorangee@gmail.com>
2023-09-23 23:04:41 +0100

    texto hero

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection = () => {
     <div className='mx-auto max-w-2xl py-10 sm:py-48'>
       <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
         <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
-          Announcing our next round of funding.{' '}
+          Lançamento caminhão Agro pelo Brasil{' '}
           <a
             href='#'
             className='font-semibold text-indigo-600'>
@@ -34,7 +34,7 @@ const HeroSection = () => {
       </div>
       <div className='text-center'>
         <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-          Data to enrich your online business
+          Agro pelo Brasil
         </h1>
         <p className='mt-6 text-lg leading-8 text-gray-600'>
           Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
